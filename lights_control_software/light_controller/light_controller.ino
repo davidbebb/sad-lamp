e23e30648010b5f9b0bcd8587da13aa945e39e2b
David Bebb <davidbebb@gmail.com>
2015-12-29 21:32:49 +0000

    first cut 8266 software - requires pin sanity check, then ready for test

--- lights_control_software/light_controller/light_controller.ino
+++ lights_control_software/light_controller/light_controller.ino
@@ -11,17 +11,32 @@ IPAddress gateway(192,168,1,1);
 IPAddress subnet(255,255,255,0);
 
 ESP8266WebServer server(80);
+
+int red = 10; 
+int blue = 9; 
+int green = 11; 
+int uv = 6;
+
+float uv_sun = 0;//0.95935;
+float b_sun =  .2;//1.00000;
+float g_sun = .6;//0.6;
+float r_sun =  1;//0.48;
  
 
 String webString="";
 
 void handle_root() {
-  server.send(200, "text/plain", "Hello. I am a light bulb");
+  server.send(200, "text/plain", "Hello. I am a light bulb. /on or /off");
   delay(100);
 }
  
 void setup(void)
 {
+  pinMode(red, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(uv, OUTPUT);
+  
   Serial.begin(115200); 
 
   
@@ -42,13 +57,15 @@ void setup(void)
    
   server.on("/", handle_root);
   
-  server.on("/temp", [](){  
-
+  server.on("/off", [](){  
+    set_brightness(0);
+    webString = 'Lights are now off';
     server.send(200, "text/plain", webString);           
   });
 
-  server.on("/humidity", [](){  
-
+  server.on("/on", [](){  
+    set_brightness(255);
+    webString = 'Lights are now on';
     server.send(200, "text/plain", webString);               
   });
   
@@ -61,4 +78,18 @@ void loop(void)
   server.handleClient();
 } 
 
+
+void set_brightness(int brightness){
+    
+  float bb = b_sun*brightness;
+  float bg = g_sun*brightness;
+  float br = r_sun*brightness;
+  float bu = uv_sun*brightness;
+  
+  analogWrite(red,(int)br);
+  analogWrite(blue,(int)bb);  
+  analogWrite(green,(int)bg);
+  analogWrite(uv, (int)bu);  
+  
+}
 
